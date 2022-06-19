28e19a5fcef6548a81293a24940d312ff495fa70
SHAXZOD <shaxzodbmaster@gmail.com>
2022-06-19 13:35:39 +0500

    TikTok error

--- download/tiktok.py
+++ download/tiktok.py
@@ -14,7 +14,7 @@ async def tiktok(message,HOST,KEY,URL):
         }
 
         response = requests.request("GET", url, headers=headers, params=querystring)
-        await message.answer_video(video = response.json()['itemData']['video']['dynamic_cover']['url_list'])
+        await message.answer_video(video = response.json()['itemData']['video']['dynamic_cover']['url_list'][0])
     except Exception as error:
         
         await message.answer('<b>{}</b> - tiktok video yuklanmadi qaytib link ni tug\'riligini tekshirib ko\'ring'.format(message.text))
